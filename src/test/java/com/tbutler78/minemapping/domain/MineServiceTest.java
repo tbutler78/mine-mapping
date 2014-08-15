210381b576513500e31f3405f0d394a97a93ffed
tbutler78 <thomas.a.butler@gmail.com>
2014-08-14 18:39:31 -0600

    Test

---
+++ src/test/java/com/tbutler78/minemapping/domain/MineServiceTest.java
@@ -0,0 +1,28 @@
+package com.tbutler78.minemapping.domain;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+
+
+@ContextConfiguration(locations={"*servlet-context.xml"})
+public class MineServiceTest {
+
+	@Test
+	public void testFindAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindByCounty() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindMine() {
+		fail("Not yet implemented");
+	}
+
+}
